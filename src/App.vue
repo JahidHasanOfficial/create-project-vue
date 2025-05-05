659a3c9967352609096c52bcd136d4f04f31656d
Jahid Hasan <jahidhasanofficial23@gmail.com>
2025-05-06 02:03:30 +0600

    bind

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,81 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+const images = [
+ {
+    image: 'https://picsum.photos/400/200?random=1',
+    alt: 'Random Image 1',
+    title: 'Card Title 1',
+    description: 'This is a description for card 1.',
+    link: 'https://example.com/card1'
+  },
+  {
+    image: 'https://picsum.photos/400/200?random=2',
+    alt: 'Random Image 2',
+    title: 'Card Title 2',
+    description: 'This is a description for card 2.',
+    link: 'https://example.com/card2'
+  },
+  {
+    image: 'https://picsum.photos/400/200?random=3',
+    alt: 'Random Image 3',
+    title: 'Card Title 3',
+    description: 'This is a description for card 3.',
+    link: 'https://example.com/card3'
+  },
+ 
+];
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+
+
+
+<section class="container py-5">
+  <div class="row g-4">
+    <!-- Card 1 -->
+    <div class="col-md-4">
+      <div class="card h-100">
+        <img :src="images[0].image" class="card-img-top" :alt="images[0].alt">
+        <div class="card-body">
+          <h5 class="card-title">{{ images[0].title }}</h5>
+          <p class="card-text">{{ images[0].description }}.</p>
+          <a :href="images[0].link" class="btn btn-primary">Learn More</a>
+        </div>
+      </div>
+    </div>
+
+    <!-- Card 2 -->
+    <div class="col-md-4">
+      <div class="card h-100">
+        <img :src="images[1].image" class="card-img-top" :alt="images[1].alt">
+        <div class="card-body">
+          <h5 class="card-title">{{ images[1].title }}</h5>
+          <p class="card-text">{{ images[1].description }}</p>
+          <a :href="images[1].link" class="btn btn-primary">Learn More</a>
+        </div>
+      </div>
+    </div>
+
+    <!-- Card 3 -->
+    <div class="col-md-4">
+      <div class="card h-100">
+        <img :src="images[2].image" class="card-img-top" alt="{{ images[2].alt }}">
+        <div class="card-body">
+          <h5 class="card-title">{{ images[2].title }}</h5>
+          <p class="card-text">{{ images[2].description }}</p>
+          <a :href="images[2].link" class="btn btn-primary">Learn More</a>
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+</section>
+
+
+
 </template>
 
+ 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
